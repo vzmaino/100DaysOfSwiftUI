fdfa1a38cacbeca0b6a7dbd2765fef5dc53f7ea5
vzmaino <vzmaino@hotmail.com>
2021-03-25 18:25:12 -0300

    wordScramble

---
+++ 7. Project 5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Vinicius Maino.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
