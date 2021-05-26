e62e27ffa39615ad5b47633e025ad268e394f6db
vzmaino <vzmaino@hotmail.com>
2021-05-25 23:01:00 -0300

    flashzilla

---
+++ 23. Project 17/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Vinicius Maino on 06/04/21.
+//
+
+import Foundation
+
+struct Card: Codable {
+    let prompt: String
+    let answer: String
+
+    static var example: Card {
+        Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
---
+++ 23. Project 17/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Vinicius Maino on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
